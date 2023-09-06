e46724cd29a3f10e51ca2ae642679e8133ca6924
MuliaHartawan <mulia.hartawa011@gmail.com>
2023-09-06 18:38:58 +0700

    feat(game): define the number of players and the number of dice

--- main.go
+++ main.go
@@ -1,6 +1,19 @@
 package main
 
-import "fmt"
-func main()  {
-	fmt.Println("Hello World")
+import (
+	"fmt"
+)
+
+func main() {
+
+	var N, M int
+	fmt.Print("Masukkan jumlah pemain: ")
+	fmt.Scan(&N)
+	fmt.Print("Masukkan jumlah dadu: ")
+	fmt.Scan(&M)
+
+	players := make([]int, N)
+	for i := range players {
+		players[i] = M
+	}
 }
